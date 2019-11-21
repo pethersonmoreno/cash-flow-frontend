f104d36469811d227c0f9b74d21a6742bb8d7740
Petherson Moreno <pethermoreno@gmail.com>
2019-11-21 01:17:47 -0300

    Updates

---
+++ precache-manifest.2c00aa200fb768879fbdf382ce95afdd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dd1a746974ab5e7d4c312ba402691a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f265bed76abbf089215d",
+    "url": "/static/css/main.291b5925.chunk.css"
+  },
+  {
+    "revision": "745313baf71e7a54adba",
+    "url": "/static/js/2.1d142968.chunk.js"
+  },
+  {
+    "revision": "f265bed76abbf089215d",
+    "url": "/static/js/main.143b6ffb.chunk.js"
+  },
+  {
+    "revision": "0303919cb05932ebb72f",
+    "url": "/static/js/runtime-main.658562b0.js"
+  }
+]);
