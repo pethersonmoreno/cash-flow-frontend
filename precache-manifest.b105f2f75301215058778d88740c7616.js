20d74922d9634e65757c9a9485cb2a710a46ce06
Petherson Moreno <pethermoreno@gmail.com>
2020-02-08 21:33:42 -0300

    Updates

---
+++ precache-manifest.b105f2f75301215058778d88740c7616.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f1ce11cb54ea005f077655ce8a6c618",
+    "url": "/cash-flow-frontend/index.html"
+  },
+  {
+    "revision": "f1fdcb51f5d6595a6659",
+    "url": "/cash-flow-frontend/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "b5e8dd27d422bd7dc4fd",
+    "url": "/cash-flow-frontend/static/css/main.e275e24c.chunk.css"
+  },
+  {
+    "revision": "f1fdcb51f5d6595a6659",
+    "url": "/cash-flow-frontend/static/js/2.32d2152e.chunk.js"
+  },
+  {
+    "revision": "2c455c36ed3d777b33737590a2cbb67e",
+    "url": "/cash-flow-frontend/static/js/2.32d2152e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b5e8dd27d422bd7dc4fd",
+    "url": "/cash-flow-frontend/static/js/main.d3437a81.chunk.js"
+  },
+  {
+    "revision": "e5ae3e1eb6bd96f807c6",
+    "url": "/cash-flow-frontend/static/js/runtime-main.2e543c99.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cash-flow-frontend/precache-manifest.8d0c616615c374b273f2b1b26705a2e5.js"
+  "/cash-flow-frontend/precache-manifest.b105f2f75301215058778d88740c7616.js"
 );
 
 self.addEventListener('message', (event) => {
